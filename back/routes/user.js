a9918a6575a4a92be34a5e4c79e02a9da294dcad
Dorian Ropert <d.ropert@bbox.fr>
2019-11-22 05:59:02 +0100

    Mise à jour compte OK

--- back/routes/user.js
+++ back/routes/user.js
@@ -21,11 +21,14 @@ route.get("/", function(req, res){
             userModel.findOne({Login:req.query.login, Password:req.query.mdp}, function(err, doc){
                 if(err) {
                     res.statusMessage = "Echec vérification identifiants";
+                    mongoose.connection.close();
                     return res.status(500).end();
                 }else{
                     if(doc) {
+                        mongoose.connection.close();
                         return res.status(200).json(doc.toJSON());
                     }else{
+                        mongoose.connection.close();
                         res.statusMessage = "Utilisateur ou mot de passe incorrect";
                         return res.status(401).end();
                     }
@@ -43,10 +46,12 @@ route.post("/", function (req, res) {
         }else{
             userModel.findOne({Login:req.body.login}, function(err, doc){
                 if(err) {
+                    mongoose.connection.close();
                     res.statusMessage = "Echec vérification login";
                     return res.status(500).end();
                 }else{
                     if(doc) {
+                        mongoose.connection.close();
                         res.statusMessage = "Utilisateur existant";
                         return res.status(409).end();
                     }else{
@@ -86,6 +91,53 @@ route.post("/", function (req, res) {
                                 });
                             }
                         });
+                    }
+                }
+            });
+        }
+    });
+});
+
+route.put("/", function (req, res) {
+    mongoose.connect("mongodb+srv://dropert:SXlUQZIM1vQfImm2@progweb-hnise.gcp.mongodb.net/cdp?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true}, function(err){
+        if(err) {
+            res.statusMessage = "Connexion BDD impossible";
+            return res.status(500).end();
+        }else{
+            userModel.findOne({Login:req.body.login}, function(err, doc){
+                if(err) {
+                    mongoose.connection.close();
+                    res.statusMessage = "Echec vérification login";
+                    return res.status(500).end();
+                }else{
+                    if(doc && req.body.login !== req.body.oldLogin) {
+                        mongoose.connection.close();
+                        res.statusMessage = "Utilisateur existant";
+                        return res.status(409).end();
+                    }else{
+                        const oldLogin = req.body.oldLogin;
+                        const nom = req.body.nom;
+                        const prenom = req.body.prenom;
+                        const mail = req.body.mail;
+                        const societe = req.body.societe;
+                        const login = req.body.login;
+                        const mdp = req.body.mdp;
+                        userModel.update({Login:oldLogin},
+                            {Nom : nom,
+                            Prenom : prenom,
+                            Mail : mail,
+                            Societe :societe,
+                            Login : login,
+                            Password : mdp}, function(err, result){
+                            if(err){
+                                mongoose.connection.close();
+                                res.statusMessage = "Echec de la mise à jour du compte";
+                                return res.status(500).end();
+                            }else {
+                                mongoose.connection.close();
+                                return res.status(200).end();
+                            }
+                            });
                     }
                 }
             });
--- front/myaccount.js
+++ front/myaccount.js
@@ -23,6 +23,7 @@ fetch(url+"?login="+login+"&mdp="+mdp)
                 document.querySelector("#prenom").value = data["Prenom"];
                 document.querySelector("#mail").value = data["Mail"];
                 document.querySelector("#login").value = data["Login"];
+                document.querySelector("#password").value = data["Password"];
                 document.querySelector("#societe").value = data["Societe"];
             });
         }else
@@ -30,3 +31,46 @@ fetch(url+"?login="+login+"&mdp="+mdp)
     }).catch(function(err){
     console.log(err.message);
 });
+
+document.querySelector("#save").addEventListener('click', onClick);
+
+function onClick()
+{
+    const url = 'http://localhost:3000/user';
+
+    const my_headers = new Headers();
+    my_headers.append("Content-Type", "application/json");
+
+    const oldLogin = readCookie("login");
+    const nom = document.getElementById('nom').value;
+    const prenom = document.getElementById('prenom').value;
+    const mail = document.getElementById('mail').value;
+    const login = document.getElementById('login').value;
+    const mdp = document.getElementById('password').value;
+    const societe = document.getElementById('societe').value;
+
+    var params = {oldLogin:oldLogin, nom:nom, prenom:prenom, mail:mail, login:login, mdp:mdp, societe:societe};
+
+    var my_init = {
+        method: 'PUT',
+        headers: my_headers,
+        mode: 'cors',
+        cache: 'default',
+        body: JSON.stringify(params)
+    };
+
+    fetch(url, my_init)
+        .then(function(res)
+        {
+            if(res.status === 409)
+                alert("Ce login est déjà utilisé, veuillez en choisir un autre");
+            else if(res.status === 200) {
+                alert("Mise à jour réussie !");
+                document.location.href = "index.html";
+            }
+            else
+                alert(res.statusText);
+        }).catch(function(err){
+        console.log(err.message);
+    });
+}
