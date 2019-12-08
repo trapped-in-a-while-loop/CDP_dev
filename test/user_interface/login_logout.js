503f2e6c0eb632506f6bbdc28624e7cd3095f06d
Pablo Tomas <pablo.tomas@etu.u-bordeaux.fr>
2019-12-08 16:16:56 +0100

    login_logout update

--- test/user_interface/login_logout.js
+++ test/user_interface/login_logout.js
@@ -3,58 +3,13 @@ const puppeteer = require("puppeteer");
 
 const url_home = "https://cdp-ropert-dupland-tomas.000webhostapp.com/";
 
-/*
-const test_login = async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.on("dialog", async dialog => {
-        await dialog.dismiss();
-    });
-    await page.goto(url_home);
-    await page.waitFor("#connexion");
-    await page.click("#connexion");
-
-    await page.waitFor("body");
-
-    await page.type("#login", "test");
-    await page.type("#password", "test");
-
-    await page.waitFor("#aut");
-    await page.click("#aut");
-    await page.waitForNavigation({waitUntil: 'load'});
-    const url_nextPage = await page.url();
-
-    assert(url_nextPage === url_home + "index.html");
-    console.log("Test log in passed");
-
-    await page.waitFor("#deconnect");
-    await page.click("#deconnect");
-
-    console.log("Test deconnect passed");
-
-    await browser.close();
-
-
-};
-
-test_login();*/
-
 let browser;
 let page;
 
-const test_init = async () => {
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-  page.on("dialog", async dialog => {
-    await dialog.dismiss();
-  });
-};
+(async () => {
 
-const test_end = async () => {
-    await browser.close();
-};
+  const test_login = async () => {
 
-const test_login = async () => {
     await page.goto(url_home);
     await page.waitFor("#connexion");
     await page.click("#connexion");
@@ -71,20 +26,24 @@ const test_login = async () => {
 
     assert(url_nextPage === url_home + "index.html");
     console.log("Test log in passed");
-};
 
-const test_logout = async () => {
+  };
+
+  const test_logout = async () => {
+
     await page.waitFor("#deconnect");
     await page.click("#deconnect");
 
     console.log("Test deconnect passed");
-};
 
-const test = async () => {
-  await test_init();
+  };
+
+  browser = await puppeteer.launch({headless : true});
+  page = await browser.newPage();
+/*  page.on("dialog", async dialog => {
+    await dialog.dismiss();
+  });*/
   await test_login();
   await test_logout();
-  await test_end();
-};
-
-test();
+  await browser.close();
+});
