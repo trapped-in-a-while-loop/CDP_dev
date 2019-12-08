6a6bb4a6f18d07bdc0d927a50c0f784313214f50
Pablo Tomas <pablo.tomas@etu.u-bordeaux.fr>
2019-12-08 14:47:54 +0100

    clean login_logout.js test

--- test/user_interface/login_logout.js
+++ test/user_interface/login_logout.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 
 const url_home = "https://cdp-ropert-dupland-tomas.000webhostapp.com/";
 
+/*
 const test_login = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -16,25 +17,18 @@ const test_login = async () => {
     await page.waitFor("body");
 
     await page.type("#login", "test");
-    console.log("login : test");
     await page.type("#password", "test");
-    console.log("password : test");
 
     await page.waitFor("#aut");
-    console.log("On attend aut");
     await page.click("#aut");
-    console.log("On clique sur connexion");
     await page.waitForNavigation({waitUntil: 'load'});
-    console.log("On attend pour naviguer");
     const url_nextPage = await page.url();
-    console.log("On récupère l'url");
 
     assert(url_nextPage === url_home + "index.html");
     console.log("Test log in passed");
 
     await page.waitFor("#deconnect");
     await page.click("#deconnect");
-    console.log("On clique sur deconnexion");
 
     console.log("Test deconnect passed");
 
@@ -43,4 +37,49 @@ const test_login = async () => {
 
 };
 
-test_login();
+test_login();*/
+
+const browser;
+const page;
+
+before(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    page.on("dialog", async dialog => {
+        await dialog.dismiss();
+    });
+});
+
+after(async () => {
+    await browser.close();
+});
+
+const test_login = async () => {
+    await page.goto(url_home);
+    await page.waitFor("#connexion");
+    await page.click("#connexion");
+
+    await page.waitFor("body");
+
+    await page.type("#login", "test");
+    await page.type("#password", "test");
+
+    await page.waitFor("#aut");
+    await page.click("#aut");
+    await page.waitForNavigation({waitUntil: 'load'});
+    const url_nextPage = await page.url();
+
+    assert(url_nextPage === url_home + "index.html");
+    console.log("Test log in passed");
+};
+
+const test_logout = async () => {
+    await page.waitFor("#deconnect");
+    await page.click("#deconnect");
+
+    console.log("Test deconnect passed");
+};
+
+test_login().then(
+  test_logout();
+);
